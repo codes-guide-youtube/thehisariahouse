bf0936805677297b2beea54602da25be7ef72343
Basic-Funda-Developer <100834471+codes-guide-youtube@users.noreply.github.com>
2023-06-18 22:13:06 +0530

    favicon addition

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="The HisariaHouse-logos_transparent.png" type="image/x-icon" />
         <title>The HisariaHouse</title>
       </head>
       <body className="flex flex-col min-h-screen">
