a1c0105a1229cd45663c5702b743a089600a9ea1
Basic-Funda-Developer <100834471+codes-guide-youtube@users.noreply.github.com>
2023-06-19 15:09:06 +0530

    removed the 'still working on this page'

--- src/app/sridhar/balance/page.js
+++ src/app/sridhar/balance/page.js
@@ -53,9 +53,6 @@ transaction.forEach((transaction) => {
       <Head>
         <title>Balance</title>
       </Head>
-      <h1 className="animate-bounce text-center m-10 py-8 px-3 border-2 border-gray-800 text-3xl">
-        Still Working On this page
-      </h1>
       <h1 className="text-center text-xl">Current Balance: ₹{balance}</h1>
       <div className="pl-3 mx-auto mt-10 grid max-w-2xl grid-cols-1 my-5 gap-x-8 gap-y-16 border-t border-gray-800 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         {transaction.map((transaction) => (
